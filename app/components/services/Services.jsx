c0507a4c7a5acc82bdfc4161d6ea6d2b24f22787
joanartdacosta <joanartdacosta@gmail.com>
2023-09-05 02:17:46 +0200

    change button style

--- app/components/services/Services.jsx
+++ app/components/services/Services.jsx
@@ -49,54 +49,71 @@ export default function Services() {
       </div>
 
       <div className={servicesStyle.service}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 id="consultation">
-            Services <span>-&gt;</span>
-          </h2>
-          <button>Schedule a consultation.</button>
+        <h2 id="consultation">Services</h2>
+        <button className={servicesStyle.buttonService}>
+          <a href="https://pt.saravedicsoul.com/book-online" target="_blank">
+            <p>Schedule a consultation.</p>
+          </a>
+        </button>
+        <p>
+          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
+          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
+          qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        </p>
 
-          <p>
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-            aliquid ex ea commodi consequatur? Quis autem vel eum iure
-            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
-            pariatur?"
-          </p>
+        <a href="https://www.saravedicsoul.com/" target="free-call">
+          <button className={servicesStyle.buttonService}>
+            Free 15min Discovery Call
+          </button>
         </a>
-        <a href="https://www.saravedicsoul.com/">
-          <button>Free 15min Discovery Call</button>
-        </a>
-        <a href="https://www.saravedicsoul.com/book-online?category=c60c3f29-4aa8-4754-b4e7-415972afa0d2">
-          <button>Vedic Astrology</button>
-        </a>
-        <a href="https://www.saravedicsoul.com/book-online?category=99d9a0c7-5218-4f40-be54-5e375eaff660">
-          <button>Ayurveda</button>
-        </a>
-        <a href="https://www.saravedicsoul.com/book-online?category=e705c6a8-3c09-4e34-ba2a-89da392f735d">
-          <button>Vedic Counselling</button>
-        </a>
-        <a href="https://www.saravedicsoul.com/book-online?category=a15ff776-55e8-4f2c-8225-8070ce3566e9">
-          <button>Holistic Programs</button>
-        </a>
-        <a href="https://www.saravedicsoul.com/book-online?category=fc6c16da-a704-4eef-86b4-31e5642bc412">
-          <button>Gift Coupon</button>
-        </a>
-        <a href="https://www.saravedicsoul.com/book-online?category=f1dcabb7-da0c-4642-b2dc-710a46cf86df">
-          <button>Counselling at Home</button>
-        </a>
+
+        <button className={servicesStyle.buttonService}>
+          <a
+            href="https://www.saravedicsoul.com/book-online?category=c60c3f29-4aa8-4754-b4e7-415972afa0d2"
+            target="vedic"
+          ></a>
+          Vedic Astrology
+        </button>
+
+        <button className={servicesStyle.buttonService}>
+          <a href="https://www.saravedicsoul.com/book-online?category=99d9a0c7-5218-4f40-be54-5e375eaff660">
+            <p>Ayurveda</p>
+          </a>
+        </button>
+
+        <button className={servicesStyle.buttonService}>
+          <a href="https://www.saravedicsoul.com/book-online?category=e705c6a8-3c09-4e34-ba2a-89da392f735d">
+            <p> Vedic Counselling</p>{" "}
+          </a>
+        </button>
+
+        <button className={servicesStyle.buttonService}>
+          <a href="https://www.saravedicsoul.com/book-online?category=a15ff776-55e8-4f2c-8225-8070ce3566e9">
+            <p>Holistic Programs</p>
+          </a>
+        </button>
+
+        <button className={servicesStyle.buttonService}>
+          <a href="https://www.saravedicsoul.com/book-online?category=fc6c16da-a704-4eef-86b4-31e5642bc412">
+            <p>Gift Coupon</p>
+          </a>
+        </button>
+
+        <button className={servicesStyle.buttonService}>
+          <a href="https://www.saravedicsoul.com/book-online?category=f1dcabb7-da0c-4642-b2dc-710a46cf86df">
+            <p>Counselling at Home</p>
+          </a>
+        </button>
       </div>
 
       <div className={servicesStyle.service}>
@@ -105,10 +122,10 @@ export default function Services() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 id="learn">
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Information about Astrology and Aruveda.</p>
+          <h2 id="learn">Learn</h2>
+          <p className={servicesStyle.titles}>
+            Information about Astrology and Aruveda.
+          </p>
         </a>
       </div>
 
@@ -119,24 +136,10 @@ export default function Services() {
           rel="noopener noreferrer"
         >
           <h2 id="contact">
-            Contact me <span>-&gt;</span>
+            <p>Contact me</p>
           </h2>
-          <p>Talk with me for collaborations, issues.</p>
-        </a>
-      </div>
-
-      <div className={servicesStyle.service}>
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL
-            with&nbsp;Vercel.
+          <p className={servicesStyle.titles}>
+            Talk with me for collaborations, issues.
           </p>
         </a>
       </div>
