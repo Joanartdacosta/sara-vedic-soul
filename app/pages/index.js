930f7031b4343733a87ed400d407fb9a3c238500
joanartdacosta <joanartdacosta@gmail.com>
2023-09-05 02:15:08 +0200

    add footer with a link

--- app/pages/index.js
+++ app/pages/index.js
@@ -5,6 +5,7 @@ import styles from "@/styles/Home.module.css";
 import NavBar from "@/components/navbar/NavBar";
 import Banner from "@/components/home/Banner";
 import Services from "@/components/services/Services";
+import LinkHome from "@/components/LinkHome";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,12 +16,16 @@ export default function Home() {
         <title>Sara Vedic Sould</title>
         <meta name="description" content="Astrology and Āyurveda vibes" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="https://joana-personal-website.s3.eu-central-1.amazonaws.com/sara-vedic-soul/logo-sembackground.png"
+        />
       </Head>
       <main>
         <NavBar />
         <Banner />
         <Services />
+        <LinkHome />
       </main>
     </>
   );
